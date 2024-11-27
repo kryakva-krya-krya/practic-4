e72894c11623b5b88f46c2009605c0c0ca781c6c
Выходи из аккаунта <nikolas130906@gmail.com>
2024-11-27 08:00:41 +0000

    redacted code

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.get("/", (req, res) => {
   res.json({
         message: "Hello World!",
-  })
-    });
+  });
+});
 
- app.listen(5050, () => console.log("Listening on http://localhost:5050"));
+app.listen(5050, () => console.log("Listening on http://localhost:5050"));
